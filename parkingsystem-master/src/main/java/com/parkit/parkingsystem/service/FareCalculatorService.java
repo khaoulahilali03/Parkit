276106bfa6fc0aa2806b28049139f6597782a939
Khaoula <hilali.khaoula@gmail.com>
2022-08-23 01:48:13 +0200

    Fix the code in FareCalculatorService.java to make the UT already present pass

--- parkingsystem-master/src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ parkingsystem-master/src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -3,6 +3,8 @@ package com.parkit.parkingsystem.service;
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
+import static java.time.temporal.ChronoUnit.MILLIS;
+
 public class FareCalculatorService {
 
     public void calculateFare(Ticket ticket){
@@ -14,15 +16,15 @@ public class FareCalculatorService {
         int outHour = ticket.getOutTime().getHours();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        double duration = MILLIS.between(ticket.getInTime().toInstant(), ticket.getOutTime().toInstant());
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                ticket.setPrice((duration/(60 * 60 * 1000)) * Fare.CAR_RATE_PER_HOUR);
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                ticket.setPrice((duration/(60 * 60 * 1000)) * Fare.BIKE_RATE_PER_HOUR);
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
